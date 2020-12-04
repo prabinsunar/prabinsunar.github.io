f1f29c5e4a647865da29878c5ae40b6ec9f81ffc
Prabin Sunar <sunarprabin.ps@gmail.com>
2020-12-04 16:41:10 +1100

    url refactoring

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PageNotFound = () => {
-	return <h1>Oops! Page not found.</h1>;
+	return <h1 className="pageNotFound">Oops! Page not found.</h1>;
 };
 
 export default PageNotFound;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,11 +8,15 @@ const Header = () => {
 	return (
 		<div className="header">
 			<nav className="navigation">
-				<NavLink to="/" activeClassName="active" className="normal" exact>
+				<NavLink to="/portfolio/" activeClassName="active" className="normal">
 					Home
 				</NavLink>
 				{' | '}
-				<NavLink to="/projects" activeClassName="active" className="normal">
+				<NavLink
+					to="/portfolio/projects"
+					activeClassName="active"
+					className="normal"
+				>
 					My Projects
 				</NavLink>
 			</nav>
