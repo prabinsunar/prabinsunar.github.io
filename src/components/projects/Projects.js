57ae1392e3f7ab75367bc266e73699105cb5916d
Prabin Sunar <sunarprabin.ps@gmail.com>
2021-06-15 20:47:46 +1000

    Rename image

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Projects.css';
 import WeatherImage from '../assets/weather-app.png';
-import RentImage from '../assets/rent-tracker.PNG';
+import RentImage from '../assets/rent-tracker.png';
 
 const Projects = () => {
 	return (
