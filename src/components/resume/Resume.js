4193b19b182d5b8a27965e74e51dcd7d4fd51923
Prabin Sunar <sunarprabin.ps@gmail.com>
2021-06-15 21:32:28 +1000

    resume image link change

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Resume.css';
-import ResumeImage from '../assets/Prabin_Sunar-resume.png';
 
 const Resume = () => {
 	return (
 		<div className="resume">
-			<img src={ResumeImage} alt="Resume" width="800" height="1032" />
+			<img
+				src="../assets/Prabin_Sunar-resume.png"
+				alt="Resume"
+				width="800"
+				height="1032"
+			/>
 			<br />
 			<a
 				href="https://drive.google.com/file/d/1xrR8Gs_Xj3OYcg0g8OzAZxVrrhTCbcQs/view?usp=sharing"
