ae382aee0c0b613ea74fe5c22114560e9e0d1aaa
Prabin Sunar <sunarprabin.ps@gmail.com>
2021-06-17 15:41:03 +1000

    logo removal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/common/Header';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Projects from './components/projects/Projects';
 import PageNotFound from './PageNotFound';
